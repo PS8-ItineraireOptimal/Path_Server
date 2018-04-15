a166e7d97a4c5fd554349500b20fa793b8d1e264
yviouswilliamous <yves.obame@yahoo.fr>
2018-04-15 22:25:56 +0200

    Test de l'algorithme de A*

---
+++ test_Astar.php
@@ -0,0 +1,50 @@
+<?php
+
+include('classes.php');
+
+?>
+
+<?php
+
+$n = array();
+$i = 1; $n[$i] = new Node($i, 3); 
+$i = $i+1; $n[$i] = new Node($i, 2); 
+$i = $i+1; $n[$i] = new Node($i, 2); 
+$i = $i+1; $n[$i] = new Node($i, 1); 
+$i = $i+1; $n[$i] = new Node($i, 1); 
+$i = $i+1; $n[$i] = new Node($i, 0);
+
+
+$a=array();
+$j=0; $a[$j]= new Arc($j,$n[1],$n[2],1);
+$j=$j+1; $a[$j]= new Arc($j,$n[1],$n[3],3);
+$j=$j+1; $a[$j]= new Arc($j,$n[2],$n[3],4);
+$j=$j+1; $a[$j]= new Arc($j,$n[3],$n[4],5);
+$j=$j+1; $a[$j]= new Arc($j,$n[2],$n[4],2);
+$j=$j+1; $a[$j]= new Arc($j,$n[2],$n[5],1);
+$j=$j+1; $a[$j]= new Arc($j,$n[5],$n[4],3);
+$j=$j+1; $a[$j]= new Arc($j,$n[5],$n[6],5);
+$j=$j+1; $a[$j]= new Arc($j,$n[4],$n[6],2);
+
+
+$g = new Graph($n,$a);
+
+/*
+print($g->find_arc($n[1],$n[3])->tps_parcours);
+
+$g->find_next_nodes($n[6]);
+
+*/
+
+$astar = new Astar($g,$n[1],$n[6]);
+
+$steps=$astar->find_best_path();
+
+print(count($steps));
+
+foreach($steps as $index => $valeur)
+{
+	print('<p>'.$valeur->id.'</p>');
+}
+
+?>
